9c6145bbd64822c3c3342912ff06aaccbb145709
Aakash Gulati <aakashgulati333@gmail.com>
2025-06-24 02:20:46 +0530

    added some cosmetic changes to the appearance and added two color modes

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
+// console.log("JS loaded!");
+// console.log("Testing button selection...");
+// const testButton = document.querySelector("#grid");
+// console.log("Grid button found:", testButton);
+
+let colorMode = 'none';
 const container = document.querySelector("#container");
-const numberOfSquares = document.querySelector("#btn");
+const shading = document.querySelector("#shading");
+const multiColor = document.querySelector("#multiColor");
+shading.addEventListener("click", () => {
+    colorMode = 'shading';
+    shading.classList.remove('selected');
+    multiColor.classList.remove('selected');
+    shading.classList.add('selected');
+});
+multiColor.addEventListener("click", () => {
+    colorMode = 'multiColor';
+    shading.classList.remove('selected');
+    multiColor.classList.remove('selected');
+    multiColor.classList.add('selected');
+});
+const numberOfSquares = document.querySelector("#grid");
 numberOfSquares.addEventListener("click",() => {
     let input = prompt("Enter the number of squares you want in a line.");
-    if (input > 1 && input <= 100) {
+    if (input >= 1 && input <= 100) {
         container.innerHTML = "";
+        colorMode = 'none';
+        shading.classList.remove('selected');
+        multiColor.classList.remove('selected');
     for (let i = 0; i <= input-1; i++){
     for (let j = 0; j <= input-1; j++){
         let square = document.createElement("div");
@@ -13,13 +36,21 @@ numberOfSquares.addEventListener("click",() => {
         square.hoverCount = 0;
         container.append(square);
         square.addEventListener("mouseenter", () => {
-            square.hoverCount++;
-            function randomColor(){
-                return Math.floor(Math.random() * 255);
+            if (colorMode === 'none'){
+                return;
             }
-            square.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
-            square.style.opacity = `${1 - (square.hoverCount * 10) / 100}`;
-        });
+            square.hoverCount++;
+            if (colorMode === 'shading'){
+                square.style.backgroundColor= "#03A6A1";
+                square.style.opacity = `${(square.hoverCount * 10) / 100}`;
+            }
+            else if (colorMode === 'multiColor'){
+                function randomColor(){
+                return Math.floor(Math.random() * 255);
+                }
+                square.style.backgroundColor = `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
+            }
+        }); 
     }
 }
     } else {
